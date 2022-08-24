d5a0d330c975866e6eb7818ebdf427f913d0330d
Yukina <yukina@yamabuki.ac.jp>
2022-08-24 18:40:33 +0900

    make random reply react to reply messages

--- handlers/imply_chain/random_reply.py
+++ handlers/imply_chain/random_reply.py
@@ -4,10 +4,12 @@ from utils.util_tg_operation import speak
 
 
 async def random_reply(client: pyrogram.Client, message: pyrogram.types.Message, rtp: float) -> bool:
-    (reply_to_possibility, reply_to_msg_id) = (rtp, message.reply_to_message.id) \
-        if message.reply_to_message else (-1, None)
+    (reply_to_possibility, reply_to_msg_id) = (rtp, message.reply_to_message.id) if message.reply_to_message else (-1, None)
     await asyncio.sleep(random.random() * 4)
     reply_list = NOTHING_REPLY_LIST
-    await speak(message.chat.id, random.choice(reply_list), message.id if random.random() < reply_to_possibility else None)
+    if reply_to_msg_id:
+        await speak(message.chat.id, random.choice(reply_list), reply_to_msg_id)
+    else:
+        await speak(message.chat.id, random.choice(reply_list), message.id if random.random() < reply_to_possibility else None)
 
     return True
